de45e5ef9d91f4017533ed4cb6d2349b40276d94
Pratham12345678 <74126953+Pratham12345678@users.noreply.github.com>
2020-11-17 18:47:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,39 @@
-var fixedRect, movingRect;
+var wall, car;
+var deformation, speed, weight;
 
 function setup() {
-  createCanvas(800,400);
-  fixedRect = createSprite(200,200,50,50);
-  fixedRect.shapeColor = "red";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "red";
-  movingRect.debug = true;
-}
+  createCanvas(1600,400);
 
+  wall = createSprite(1500, 200, 60, height/2);
+  wall.shapeColor = "brown";
+
+  car = createSprite(50, 200, 50, 50);
+  car.shapeColor = "white";
+  car.velocityX = speed;
+
+  speed = Math.round(random(55, 90));
+  weight = Math.round(random(400, 1500));
+
+  deformation = 0.5*speed*speed/22500;
+ 
 function draw() {
-  background("lightblue");  
+  background(255,255,255); 
 
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-
-  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 &&
-    fixedRect.x - movingRect.x  < fixedRect.width/2 + movingRect.width/2 &&
-    movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 &&
-    fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
-    movingRect.shapeColor = 'purple';
-    fixedRect.shapeColor = 'purple';
-  }
-  else{
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
+  if(wall.x-car.x < (car.width+wall.width)){
+    if(deformation<100){
+      car.shapeColor = "green";
+    }
+  
+    if(deformation>100 && deformation<180){
+      car.shapeColor = "yellow";
+    }
+  
+    if(deformation>180){
+      car.shapeColor = "red";
+    }
+    }
   }
   
+
   drawSprites();
-
-  
 }
